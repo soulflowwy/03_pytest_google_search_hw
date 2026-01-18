1092180e8ca37c410d956fe0b2aedf6e1fa1b15b
Danil Reznikov <solomonheadquaters@gmail.com>
2026-01-18 15:25:14 +0500

    test: add negative google search case

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,6 +1,11 @@
 from selene import browser, be, have
 
 def test_google_search(browser_set_up):
-    browser.open('https://google.com')
+    browser.open('/')
     browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
     browser.element('html').should(have.text('About this page'))
+
+def test_google_search_negative(browser_set_up):
+    browser.open('/')
+    browser.element('[name="q"]').should(be.blank).type(';;;;;;sadjdsakdk asdjkas kds kjasd sadldjkl jasd ------------').press_enter()
+    browser.element('body').should(have.text('Your search - ;;;;;;sadjdsakdk asdjkas kds kjasd sadldjkl jasd ------------ - did not match any documents.'))
